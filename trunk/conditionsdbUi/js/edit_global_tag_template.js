d25aa59a496be84334d72cd429638523746e377e
Eric Stephan <Eric.Stephan@pnnl.gov>
2015-10-14 22:09:57 +0000

    Added validation logic to global tag name fields in create and edit forms.
    git-svn-id: https://subversion.pnnl.gov/svn/belle2db@102 07a2e922-e25d-4af6-bb19-728dab94992a

--- trunk/conditionsdbUi/js/edit_global_tag_template.js
+++ trunk/conditionsdbUi/js/edit_global_tag_template.js
@@ -1,17 +1,29 @@
 function set_edit_global_tag_tmpl (types, globalId) {  
+
+
+
+
  edit_global_tag_tmpl =  '   <form role = "form" id="frm_edit_global_tag" > '
       +  '   <div class="panel panel-default"> '
       +  '   <div class="panel-heading">Edit Global Tag</div> '
       +  '   <div class="panel-body" > '
+      + ' <div class="form-group"> '
       +  '   <input type="hidden" name="globaltagid" value="' + globalId + '"/>'
       +  '   <label id="label_gtname" for="input_gtname" valign="top">Name:</label> '
-      +  '   <input id="input_gtname" class="form-control" placeholder="Enter name"/> '
+      +  '   <input id="input_gtname" pattern="^[_A-z0-9\-]{1,}$"  class="form-control" placeholder="Enter name" data-error="Only use alphanumeric characters, underscores, and hyphens in name" required/> '
+      + '  <span class="glyphicon form-control-feedback glyphicon-remove" aria-hidden="true"></span> '
+      + '  <span class="help-block with-errors">Only use alphanumeric characters, underscores, and hyphens in name</span> '
+      +  '   </div> '
+      + ' <div class="form-group"> '
       +  '   <label id="label_gttype" for="select_gttype" valign="top">Type:</label> '
       +  '  <select class="form-control" id="select_gttype"> '
       +  gt_types 
       +  ' </select> '
+      +  '   </div> '
+      + ' <div class="form-group"> '
       +  '   <label id="label_gtdesc" for="textarea_gtdesc" valign="top">Description:</label> '
       +  '   <textarea class="form-control" rows="5" id="textarea_gtdesc" placeholder="Description required"></textarea> '
+      +  '   </div> '
       +  '   <button type="submit" class="btn btn-default" onclick="(modify_global_tag(input_gtname.value , textarea_gtdesc.value , select_gttype.value, globaltagid.value ))"> '
       +  '   Modify</button>  '
       +  '   <button type="cancel" class="btn btn-default" onclick="( clear_right_div ())"> '
@@ -20,6 +32,42 @@ function set_edit_global_tag_tmpl (types, globalId) {
       +  '   </div> '
       +  '  </form> ';
   }
+
+
+
+ create_global_tag_tmpl =  '   <form data-toggle="validator" role="form"  id="frm_create_global_tag" > '
+  +  '   <div class="panel panel-default"> '
+  +  '   <div class="panel-heading">Create Global Tag</div> '
+  +  '   <div class="panel-body" > '
+  + ' <div class="form-group"> '
+  +  '<label id="label_gtname" for="input_gtname" valign="top" class="control-label">Name:</label> '
+  + ' <input type="text"  pattern="^[_A-z0-9\-]{1,}$" class="form-control" id="input_gtname" placeholder="Global Tag Name" data-error="Only use alphanumeric characters, underscores, and hyphens in name" required> '
+  + '  <span class="glyphicon form-control-feedback glyphicon-remove" aria-hidden="true"></span> '
+  + '  <span class="help-block with-errors">Only use alphanumeric characters, underscores, and hyphens in name</span> '
+  + ' </div> '
+  + ' <div class="form-group"> '
+  +  '   <label id="label_gttype" for="select_gttype" valign="top">Type:</label> '
+  +  '  <select class="form-control" id="select_gttype"> '
+  +  gt_types
+  +  ' </select> '
+  + ' </div> '
+  + ' <div class="form-group"> '
+  +  '   <label id="label_gtdesc" for="textarea_gtdesc" valign="top">Description:</label> '
+  +  '   <textarea class="form-control" rows="5" id="textarea_gtdesc" placeholder="Description required"></textarea> '
+  + ' </div> '
+  + ' <div class="form-group"> '
+  +  '   <button type="submit" class="btn btn-default" onclick="(new_global_tag(input_gtname.value , textarea_gtdesc.value , select_gttype.value ))"> '
+  //+  '   <button type="submit" class="btn btn-primary" onclick="alert(input_gtname.value)"> '
+  +  '   Create</button>  '
+  +  '   <button type="cancel" class="btn btn-default" onclick="( clear_right_div ())"> '
+  +  '   Cancel</button>  '
+  + ' </div> '
+  + ' </div> '
+  + ' </div> '
+  + ' </form>';
+
+
+
 
   function modify_global_tag(name,description,type,globalTagId) {
      var gt_type_list = new Object();
